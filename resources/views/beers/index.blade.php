bf14c9583f7ecce1c67d50f584f55613f468a639
MarcoB <mrc.bsllt@gmail.com>
2021-02-10 16:01:32 +0100

    creato la tabella

--- resources/views/beers/index.blade.php
+++ resources/views/beers/index.blade.php
@@ -19,5 +19,34 @@
 
 @section("content")
   <main id=table_section>
+
+    <table class="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>BRAND</th>
+          <th>TYPE</th>
+          <th>DESCRIPTION</th>
+          <th>ALCOHOL CONTENT</th>
+          <th>PRICE</th>
+          <th>CREATED AT</th>
+          <th>UPDATED AT</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($beers as $beer)
+          <tr>
+            <td>{{ $beer->id }}</td>
+            <td>{{ $beer->brand }}</td>
+            <td>{{ $beer->type }}</td>
+            <td>{{ substr($beer->description, 0, 15)."..." }}</td>
+            <td>{{ $beer->alcohol_content }}</td>
+            <td>{{ $beer->price }}</td>
+            <td>{{ $beer->created_at }}</td>
+            <td>{{ $beer->updated_at }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
   </main>
 @endsection
