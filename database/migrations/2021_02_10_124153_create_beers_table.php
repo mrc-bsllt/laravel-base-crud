ab0482672cd6048c770c12053be3d1eb3a530628
MarcoB <mrc.bsllt@gmail.com>
2021-02-10 14:00:10 +0100

    primo commit, creato progetto laravel, database e migrato la colonna beers

---
+++ database/migrations/2021_02_10_124153_create_beers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 50);
+            $table->string('type', 30);
+            $table->text('description', 30);
+            $table->string('alcohol_content', 5);
+            $table->float('price', 4, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
