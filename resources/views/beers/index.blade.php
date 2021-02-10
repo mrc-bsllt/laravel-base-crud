e392d186368b5f14b690f2419edcd43c759395f9
MarcoB <mrc.bsllt@gmail.com>
2021-02-10 15:23:18 +0100

    installato Bootstrap e collegato i due css

--- resources/views/beers/index.blade.php
+++ resources/views/beers/index.blade.php
@@ -2,9 +2,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/app') }}">
+    <link rel="stylesheet" href="{{ asset('css/style') }}">
     <title>Prova</title>
   </head>
   <body>
-    
+    <header>
+
+    </header>
   </body>
 </html>
