242eaa2b0dba0d6eff8bf60cacbe98d3776129c7
MarcoB <mrc.bsllt@gmail.com>
2021-02-11 14:05:27 +0100

    modifiato il layout della vista show e modificata la struttura di composizione dell'html

--- resources/views/beers/index.blade.php
+++ resources/views/beers/index.blade.php
@@ -8,37 +8,26 @@
   <main id=table_section>
 
     <table class="table table-dark table-striped table-bordered">
+
       <thead>
         <tr>
-          {{-- <th>ID</th>
-          <th>BRAND</th>
-          <th>TYPE</th>
-          <th>DESCRIPTION</th>
-          <th>ALCOHOL CONTENT</th>
-          <th>PRICE</th>
-          <th>CREATED AT</th>
-          <th>UPDATED AT</th>
-          <th>LINK</th> --}}
           @foreach ($beers->toArray()[0] as $key => $value)
             <th>{{ $key }}</th>
           @endforeach
         </tr>
       </thead>
+
       <tbody>
-        @foreach ($beers as $beer)
+        @foreach ($beers->toArray() as $beer)
           <tr>
-            <td>{{ $beer->id }}</td>
-            <td>{{ $beer->brand }}</td>
-            <td>{{ $beer->type }}</td>
-            <td>{{ substr($beer->description, 0, 15)."..." }}</td>
-            <td>{{ $beer->alcohol_content }}</td>
-            <td>{{ $beer->price }}</td>
-            <td>{{ $beer->created_at }}</td>
-            <td>{{ $beer->updated_at }}</td>
-            <td><a href="{{ route('beers.show', ['beer' => $beer->id]) }}">Dettaglio</a></td>
+            @foreach ($beer as $key => $value)
+              <td>{{ $key == "description" ? substr($value, 0, 15)."...": $value }}</td>
+            @endforeach
+            <td><a href="{{ route('beers.show', ['beer' => $beer["id"]]) }}">Dettaglio</a></td>
           </tr>
         @endforeach
       </tbody>
+      
     </table>
   </main>
 @endsection
--- resources/views/beers/show.blade.php
+++ resources/views/beers/show.blade.php
@@ -8,33 +8,12 @@
   <main id="show_detail">
 
     <table class="table table-dark table-striped table-bordered">
-      <thead>
-        <tr>
-          {{-- <th>ID</th>
-          <th>BRAND</th>
-          <th>TYPE</th>
-          <th>DESCRIPTION</th>
-          <th>ALCOHOL CONTENT</th>
-          <th>PRICE</th>
-          <th>CREATED AT</th>
-          <th>UPDATED AT</th> --}}
-          @foreach ($beer->toArray() as $key => $value)
-            <th>{{ $key }}</th>
-          @endforeach
-        </tr>
-      </thead>
-      <tbody>
-          <tr>
-            <td>{{ $beer->id }}</td>
-            <td>{{ $beer->brand }}</td>
-            <td>{{ $beer->type }}</td>
-            <td>{{ substr($beer->description, 0, 15)."..." }}</td>
-            <td>{{ $beer->alcohol_content }}</td>
-            <td>{{ $beer->price }}</td>
-            <td>{{ $beer->created_at }}</td>
-            <td>{{ $beer->updated_at }}</td>
-          </tr>
-      </tbody>
+      @foreach ($beer->toArray() as $key => $value)
+      <tr>
+        <th>{{ $key }}</th>
+        <td>{{ $value }}</td>
+      </tr>
+    @endforeach
     </table>
 
   </main>
